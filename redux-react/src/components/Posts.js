74ee4ff7a45799f342e56a3e82526d940482f784
Saharsh <mac@Saharshs-MacBook-Pro.local>
2020-05-06 17:33:15 +0530

    reduxs.

--- redux-react/src/components/Posts.js
+++ redux-react/src/components/Posts.js
@@ -1,37 +1,52 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Posts extends Component {
+import { fetchPosts } from '../actions/postActions'
 
-    constructor(props){
-        super(props);
-        this.state = {
-            posts : []
+class Posts extends Component {
+
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+    componentDidUpdate(nextProps) {
+        if (nextProps.newPost) {
+            this.props.posts.unshift(nextProps.newPost);
         }
     }
 
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(res => res.json())
-            .then(data=> this.setState({posts: data}));
-    }
-
-
 
     render() {
-        const postItems = this.state.posts.map(post => (
-            <div key = {post.id}>
+        const postItems = this.props.posts.map(post => (
+            <div key={post.id}>
                 <h3> {post.title}</h3>
                 <p> {post.body}</p>
 
             </div>
         )
-            )
+        )
+
         return (
             <div>
                 <h1> Posts</h1>
                 {postItems}
-                
+
             </div>
         )
     }
 }
+
+Posts.propTypes = {
+    fetchPosts: PropTypes.func.isRequired,
+    posts: PropTypes.array.isRequired,
+    newPost: PropTypes.object
+};
+
+const mapStateToProps = state => ({
+    posts: state.posts.items,
+    newPost: state.posts.item
+})
+
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
+
--- redux-react/src/reducers/postReducer.js
+++ redux-react/src/reducers/postReducer.js
@@ -1,7 +1,25 @@
-import {FETCH_POSTS,NEW_POSTS} from '../actions/types';
+import {FETCH_POSTS,NEW_POST} from '../actions/types';
 
 
 const initialState = {
     items: [],
     item:{}
+}
+
+export default function(state = initialState, action){
+    switch(action.type){
+
+        case FETCH_POSTS:
+            return{
+                ...state,
+                items: action.payload
+            };
+        case NEW_POST:
+            return{
+                ...state,
+                items: action.payload
+            };
+        default:
+            return state;
+    }
 }
